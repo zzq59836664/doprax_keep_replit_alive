eb647df483104828197fa55bd3a648c1a0284094
zzq59836664 <119065798+zzq59836664@users.noreply.github.com>
2022-12-27 21:46:06 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var http = require('http')
 var request = require('request');
 
 // 这里填你的replit应用的url，多个url用逗号隔开
-let replit_app_urls = ["https://vv.mrzyang.repl.co", "https://replit-trojan.yangzhang23.repl.co"]
+let replit_app_urls = ["https://vv.mrzyang.repl.co", "https://replit-trojan.yangzhang23.repl.co", "https://v2ray.zzq59836664.repl.co/"]
 
 var port = 4000
 var server = http.createServer(function (request, response) {
